2375d9bdf05b6e13df4b1a85dbbcac09eed19921
Alexander Hansen <ahansen1335@gmail.com>
2024-10-02 16:32:53 +0200

    fix literally everything preventing asteroids from spawning

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
 import pygame
-import constants
-import player
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((
-        constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
-    x = constants.SCREEN_WIDTH / 2
-    y = constants.SCREEN_HEIGHT / 2
-    player_character = player.Player(x, y, constants.PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player_character.draw(screen)
-        player_character.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
+
