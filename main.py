ec825bfb395b00ea85d9132ef96967671ef19e08
Alexander Hansen <ahansen1335@gmail.com>
2024-10-02 15:26:00 +0200

    add player movement left and right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+import player
 
 
 def main():
@@ -8,14 +9,19 @@ def main():
         constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+    player_character = player.Player(x, y, constants.PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_character.draw(screen)
+        player_character.update(dt)
         pygame.display.flip()
-        dt = clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
